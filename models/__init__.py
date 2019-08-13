9ea063855aa6caafcf19fad148899e10c5dcb3ed
13952522076 <melody@dynamic-10-125-247-107.dynamic.unt.edu>
2019-08-12 21:21:21 -0500

    init

---
+++ models/__init__.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import
+
+from .resnet_old import *
+from .resnet_sge import *
+from .resnet_se  import *
+from .resnet_cbam  import *
+from .resnet_bam  import *
+from .resnet_sk  import *
+
+from .resnet_se_res import *
+from .resnet_bam_res import *
+from .resnet_cbam_res import *
+
+
+from .resnet_ce import *
+from .resnet_re import *
---
+++ models_bak/__init__.py
@@ -0,0 +1,12 @@
+#from torchvision.models.resnet import *
+from .resnet import *
+from .resnet3 import *
+from .dpn import *
+from .darknet import *
+from .se_resnet import *
+from .se_resnext import *
+from .pre_resnet import *
+from .resnext import *
+from .inceptionresnetv2 import *
+from .inceptionv4 import *
+
